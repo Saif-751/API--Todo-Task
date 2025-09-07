00160404e59f974dde51d67c979c285a2a2f7f9a
Saif-751 <sharifsaif751@gmail.com>
2025-09-07 15:19:51 +0500

    assd

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -19,7 +19,6 @@ const Profile = ({ navigation }) => {
     try {
       const response = await Index.get("/product");
       setProducts(response.data);
-      console.log("res", response.data);
     } catch (error) {
       console.log("wrong something", error);
     } finally {
